f8480a86918ba06a93f8feda038000c774f637b7
Anwesh43 <anweshthecool0@gmail.com>
2018-09-20 11:27:13 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedincompletepieview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedincompletepieview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedincompletepieview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.incompletepieview.IncompletePieView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        IncompletePieView.create(this)
     }
 }
