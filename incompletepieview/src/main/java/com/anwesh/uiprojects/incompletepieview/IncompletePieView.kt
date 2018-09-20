69a22abf318c0df846341a234e673c2add65c25c
Anwesh43 <anweshthecool0@gmail.com>
2018-09-20 11:04:11 +0530

    adding drawIPNode canvas extension methid

--- incompletepieview/src/main/java/com/anwesh/uiprojects/incompletepieview/IncompletePieView.kt
+++ incompletepieview/src/main/java/com/anwesh/uiprojects/incompletepieview/IncompletePieView.kt
@@ -14,6 +14,23 @@ import android.graphics.Color
 
 val nodes : Int = 5
 
+fun Canvas.drawIPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / 3
+    paint.color = Color.parseColor("#4527A0")
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(scale - 0.5f, 0f)) * 2
+    save()
+    translate(gap + i * gap, h/2 + (h/2 - size) * sc2)
+    drawArc(RectF(-size, -size, size, size), i * gap, 60f + 300f * sc1, false, paint)
+    restore()
+}
+
 class IncompletePieView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
