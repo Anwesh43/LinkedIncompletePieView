a36fdfbe623caba20e689160a4ed3820f4492e28
Anwesh43 <anweshthecool0@gmail.com>
2018-09-20 11:09:52 +0530

    adding LinkedIP class

--- incompletepieview/src/main/java/com/anwesh/uiprojects/incompletepieview/IncompletePieView.kt
+++ incompletepieview/src/main/java/com/anwesh/uiprojects/incompletepieview/IncompletePieView.kt
@@ -138,4 +138,27 @@ class IncompletePieView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedIP(var i : Int) {
+        private var root : IPNode = IPNode(0)
+        private var curr : IPNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+    }
 }
