dc045f525e60e02e729e46c41cb9eb7ac6c4e368
Anwesh43 <anweshthecool0@gmail.com>
2018-09-20 11:07:59 +0530

    adding IPNode class

--- incompletepieview/src/main/java/com/anwesh/uiprojects/incompletepieview/IncompletePieView.kt
+++ incompletepieview/src/main/java/com/anwesh/uiprojects/incompletepieview/IncompletePieView.kt
@@ -94,4 +94,48 @@ class IncompletePieView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class IPNode(var i : Int, val state : State = State()) {
+
+        private var next : IPNode? = null
+        private var prev : IPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = IPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawIPNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : IPNode {
+            var curr : IPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
